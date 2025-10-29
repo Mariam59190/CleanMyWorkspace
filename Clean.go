25e9da636f3044675ef29a3013975231e976b58b
Mariam59190 <mariamwalamako@gmail.com>
2025-10-29 20:17:26 +0100

    Implement CleanWorkSpace function
    Add CleanWorkSpace function to clear workspace data

--- Clean.go
+++ Clean.go
@@ -1,1 +1,11 @@
+package Clean
+
+func CleanWorkSpace(workspace *[][]*string) *[][]*string {
+    for i := range *workspace {
+        for j := range (*workspace)[i] {
+            (*workspace)[i][j] = nil
+        }
+    }
+    return workspace
+}
 
